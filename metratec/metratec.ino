81a9b9e98ced5b8e6d6d9868a6d10f500454aec9
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2023-10-02 10:52:46 +0200

    update

--- metratec/metratec.ino
+++ metratec/metratec.ino
@@ -1,8 +1,32 @@
-void setup() {
+/*
+@author Abdulrahman Othman
+@email  abdulrahman.othman@web.de
+@date   25.09.2023
+@brief  Blind UHF Project
+@device Rasp Pi Pico W
+@UHF    MetraTec
+*/
 
+#define RD 0x28;
+const byte read_f[] = {0x52, 0x02};
+
+void setup()
+{
+  Serial1.setTimeout(10);
+  Serial1.begin(115200); // Change the baud rate to match your sensor's specification
+  Serial.begin(115200);
+  Serial.println("Restting Reader ][ "+Serial1.readString()); 
+  Serial.println("STD ETS ][ "+Serial1.readString()); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  //Serial1.write(read_f, sizeof(read_f));
+  Serial1.write(0x28);
+  Serial.println(Serial1.readString());
+  //if (Serial1.available())
+  //{
+  //}
+  delay(500);
+  //Serial.println(MSG_CRC_INIT);
 }
