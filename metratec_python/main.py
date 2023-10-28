ed02eb75892bd03ddc6b5e15b7848adeada7e639
Abdulrahman Othman <abdulrahman.othman@ges.thm.de>
2023-10-28 02:05:45 +0200

    update vars, consts

--- metratec_python/main.py
+++ metratec_python/main.py
@@ -1,24 +1,68 @@
-from machine import Pin, ADC
+'''
+@author Abdulrahman Othman
+@email  abdulrahman.othman@web.de
+@date   25.10.2023
+@brief  Blind UHF Project
+@device Rasp Pi Pico W MicroPython
+@UHF    MetraTec
+'''
+
+# MicroPython Moduls
+from machine import UART,Pin
 import bluetooth
 from ble_simple_peripheral import BLESimplePeripheral
-import time
+from time import sleep
+
+# Metra Commands
+metrca = {
+    "reset": bytearray([0x52, 0x53, 0x54, 0x0D]),
+    "ets": bytearray([0x53, 0x54, 0x44, 0x20, 0x45, 0x54, 0x53, 0x0D]),
+    "sriOn": bytearray([0x53, 0x52, 0x49, 0x20, 0x4F, 0x4E, 0x0D]),
+    "inv": bytearray([0x49, 0x4E, 0x56, 0x0D]),
+    "setMsk": bytearray([0x53, 0x45, 0x54, 0x20, 0x4D, 0x53, 0x4B]),
+    "tagRead": bytearray([0x52, 0x44, 0x54, 0x20, 0x54, 0x49, 0x44]),
+    "tagRead1": bytearray([0x52, 0x44, 0x54, 0x20, 0x54, 0x49, 0x44, 0x20, 0x30, 0x20, 0x30])
+}
 
 # Create a Bluetooth Low Energy (BLE) object
 ble = bluetooth.BLE()
 
 # Create an instance of the BLESimplePeripheral class with the BLE object
 sp = BLESimplePeripheral(ble)
-adc = ADC(4)
-a = 1
+
+# UART0 initialisieren for MetraTec Module
+uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1), bits=8, parity=None, stop=1)
+print('Started UART0:', uart0)
+
+# Pins
+# Vibration
+vibration = False
+vibPin = 11
+# Ton
+tonPin = 9
+
+# Consts
+respo = "Response:"
+
+#Setups
+uart0.write(metrca['reset'])
+sleep(0.1)
+print(respo + "RST="+ uart0.readline())
+
+uart0.write(metrca['ets'])
+sleep(0.1)
+print(respo + "STD ETS="+ uart0.readline())
+
+uart0.write(metrca['sriOn'])
+sleep(0.1)
+print(respo + "SRI ON="+ uart0.readline())
+
 while True:
     if sp.is_connected(): # Check if a BLE connection is established
         # Read the value from the internal temperature sensor
-        temperature = adc.read_u16() * 3.3 / (65535 * 0.8)
-
-        # Transmit the temperature value over BLE
-        temperature_data = str(temperature).encode()
         
         sp.send("1111111")
-        a = a+1
-        print(a)
-    time.sleep(3)
+        uart0.write(reset)
+        sleep(0.5)
+        print(uart0.read())
+        sleep(3)
