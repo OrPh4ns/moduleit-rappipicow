4b3c66a8dfe14edd1933bb5f94240bb2e4b2197a
Abdulrahman Othman <abdulrahman.othman@ges.thm.de>
2023-11-11 23:20:01 +0100

    update new class

--- metratec_python/main.py
+++ metratec_python/main.py
@@ -3,39 +3,33 @@ init update later
 '''
 from machine import UART, Pin
 from time import sleep
+from .reader import Reader
 
 led_onboard = Pin("LED", Pin.OUT)
 
 respo = "Response:"
-reset = [0x52 ,0x53 ,0x54 ,0x0D]
-
+reset = bytearray([0x52, 0x53, 0x54, 0x0D])
 ets = bytearray([0x53, 0x54, 0x44, 0x20, 0x45, 0x54, 0x53, 0x0D])
 sriOn = bytearray([0x53, 0x52, 0x49, 0x20, 0x4F, 0x4E, 0x0D])
 inv = bytearray([0x49, 0x4E, 0x56, 0x0D])
 
-the_text = "0x52 0x53 0x54 0x0D"
 
-# UART0 initialisieren
-uart0 = UART(0, baudrate=115200, tx=Pin(0), rx=Pin(1), bits=8, parity=None, stop=1)
+from machine import UART, Pin
+from time import sleep
+import binascii
+
+
+reader = Reader(115200,12,1)
+uart0 = UART(0, baudrate=115200, tx=Pin(12), rx=Pin(1) ,bits=8, parity=None, stop=1)
 
 
 while True:
-    led_onboard.on()
-    # halbe Sekunde warten
-    sleep(0.5)
-    # LED ausschalten
-    led_onboard.off()
-    uart0.write("0x52 0x53 0x54 0x0D")
+    #binascii.hexlify("5253540D")
+
+    uart0.write()
+    sleep(0.1)
     if uart0.any():
         print(uart0.read())
-        sleep(1)
-    print("Connected..")
-    uart_read = uart0.read()
-    if uart_read is not None:
-        if "OK" in uart_read:
-            uart0.write(''' xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxs ''')
-            uart0.read()
-        uart0.write(uart_read)
-        uart0.read()
-    else:
-        print(uart0.read())
+    sleep(0.3)
+    
+    "52 53 54 0D"
---
+++ metratec_python/reader.py
@@ -0,0 +1,43 @@
+'''
+@author Abdulrahman Othman
+@email  abdulrahman.othman@web.de
+@date   09.11.2023
+@brief  Blind UHF Project
+@device Rasp Pi Pico W MicroPython
+@UHF    MetraTec
+'''
+from machine import UART, Pin
+
+# Reader Commands (Constants)
+metrca = {
+    "reset": bytearray([0x52, 0x53, 0x54, 0x0D]),
+    "ets": bytearray([0x53, 0x54, 0x44, 0x20, 0x45, 0x54, 0x53, 0x0D]),
+    "sriOn": bytearray([0x53, 0x52, 0x49, 0x20, 0x4F, 0x4E, 0x0D]),
+    "inv": bytearray([0x49, 0x4E, 0x56, 0x0D]),
+    "setMsk": bytearray([0x53, 0x45, 0x54, 0x20, 0x4D, 0x53, 0x4B]),
+    "tagRead": bytearray([0x52, 0x44, 0x54, 0x20, 0x54, 0x49, 0x44]),
+    "tagRead1": bytearray([0x52, 0x44, 0x54, 0x20, 0x54, 0x49, 0x44, 0x20, 0x30, 0x20, 0x30])
+}
+
+class Reader:
+    counter = 0
+    def __init__(self, baudrate:int, tx_pin:int, rx_pin:int):
+        self.uart = UART(0, baudrate=baudrate, tx=Pin(tx_pin), rx=Pin(rx_pin))
+
+    def reset(self):
+        print("Hello my name is " + self.name)
+
+    def read_tag(self):
+        pass
+
+    def prepare(self):
+        pass
+
+    def sleep(self):
+        pass
+
+    def wake_up(self):
+        pass
+
+    def response_print(self, msg:str, reponse):
+        pass
