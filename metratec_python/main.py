06a78b8b0228522966a3f50f8a7a264587ef32ad
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2023-12-05 15:32:29 +0100

    update getting data

--- metratec_python/main.py
+++ metratec_python/main.py
@@ -16,6 +16,7 @@ from reader import Reader
 # Objects
 power_button = Pin(15, Pin.IN, Pin.PULL_DOWN)
 buzzer_pin =Pin(14, Pin.OUT)
+vibration_pin =Pin(10, Pin.OUT,  value=0)
 buzzer = PWM(buzzer_pin)
 # Create a Bluetooth Low Energy (BLE) object
 ble = BLE()
@@ -39,6 +40,9 @@ def play_tone(frequency, duration, duty):
     sleep_ms(duration)
 
 while True:
+    repeat = 3
+
+
     if power_button.value():
         if xPower:
             sleep(0.5)
@@ -47,19 +51,31 @@ while True:
             sp.desonnect()
             reader.sleep()
             play_tone(300, 250, 4000)
+            # for _ in range (repeat*2):
+            #         vibration_pin.toggle()
+            #         sleep(0.2)
+            # Turn on the vibration module
+            vibration_pin.toggle()
+            sleep(0.1)
+            #vibration_pin.off()
             play_tone(250, 200, 4000)
             play_tone(200, 150, 4000)
+            vibration_pin.off()
         else:
             sleep(0.5)
             xPower=True
             sp.connect()
             reader.wake_up()
             play_tone(200, 100, 4000)
+            vibration_pin.toggle()
             play_tone(250, 200, 4000)
             play_tone(300, 300, 4000)
-            
+            vibration_pin.off()
     if xPower:
-        # if sp.is_connected():
-        reader.read_epc()
+        r = reader.read_epc()
+        if sp.is_connected() and r:
+            sp.send(r)
+        if "INV" in r:
+            print(r)
         #sp.send(str(reader.read_epc()))
         #sleep(0.5)
